289313fac441d2cfbe04203e07582282dc71313b
mert coskun <mertt.coskunm@bitbucket.org>
2025-04-14 16:19:48 +0200

    was made first connection to mongoDB

--- server.js
+++ server.js
@@ -1,17 +1,53 @@
 const express = require('express');
-const keys = require('./conig/keys.js');
+const keys = require('./config/keys.js');
 
 const app = express();
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect(keys.mongoURI);
 
 
-app.get('/auth', async (req, res) => {
-    console.log(req.params);
-    res.send("Hello world! It is "+ Date.now());
+require('./models/Account');
+const Account = mongoose.model('accountSchema');
 
+
+app.get('/account', async (req, res) => {
+    const {rUsername, rPassword} = req.query; 
+
+    console.log(rUsername);
+    console.log(rPassword);
+
+
+    var userAccount = await Account.findOne({username: rUsername}); 
+
+    if(userAccount == null )
+    {
+        console.log("Create new account"); 
+
+        var newAccount = new Account({
+            username : rUsername,
+            password : rPassword,
+            
+            lastAuthentication: Date.now()
+
+        });
+
+        await newAccount.save();
+        
+        res.send(newAccount);
+        return;
+    }else{
+        if(rPassword == userAccount.password)
+        {
+            userAccount.lastAuthentication = Date.now();
+            await userAccount.save();
+            
+            res.send(userAccount);
+            console.log("burdayımmm burdaaa ");
+            return;  
+        }
+    }
 });
 
 
