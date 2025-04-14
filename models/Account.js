8a7ccd32f4c521f6c756e1d62e80804cd30e514b
mert coskun <mertt.coskunm@bitbucket.org>
2025-04-14 16:19:21 +0200

    created account model

---
+++ models/Account.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+
+const accountSchema = new Schema({
+
+    username: String,
+    password: String,
+
+    lastAuthentication: Date,
+})
+
+
+
+mongoose.model('accountSchema', accountSchema); 
+ 
